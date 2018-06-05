2458214e27378e6b9291c6fc8dd1f5fb4865ff75
Huynh Phi Hoang <choidfchoidf@yahoo.com.vn>
2018-06-06 00:13:47 +0700

    BT section 2

---
+++ Bt Section 1.2/Bt Section 1.2.cpp
@@ -0,0 +1,55 @@
+//Exercise 1.3
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "Hello World";
+//	return 0;
+//}
+
+
+//Exercise 1.4
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    cout << "Enter two numbers:" << endl;
+//	int v1 = 0, v2 = 0;
+//	cin >> v1 >> v2;
+//	cout << "The product of " << v1 << " and " << v2
+//	<< " is " << v1 * v2 << endl;
+//	return 0;
+//}
+
+
+//Exercise 1.5
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Enter two numbers:" << std::endl;
+    int v1 = 0,v2 = 0;
+    cin >> v1;
+    cin >> v2;
+    cout << "The product of ";
+    cout << v1;
+    cout << " and ";
+    cout << v2;
+    cout << " is ";
+    cout << v1 * v2;
+    cout << endl;
+    return 0;
+}
+
+
+//Exercise 1.6 không dung, sua bang cach them cout vao dau dong
+
+//cout << "The sum of " << v1;
+//cout << " and " << v2;
+//cout << " is " << v1 + v2 << std::endl;
